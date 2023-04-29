f07c15efb67d0c68503b33bf5d3a130afb4f10ee
lu.garcia <lu.garcia@alumno.um.edu.ar>
2023-04-29 18:01:22 -0300

    entrega tp1

--- tp1/mirror.py
+++ tp1/mirror.py
@@ -1,42 +1,41 @@
 import os
 import argparse
+import time
 
 
 def mirror_text():
     hijo_read, padre_write = os.pipe()
     padre_read, hijo_write = os.pipe()
 
-    try:
-        parser = argparse.ArgumentParser()
-        parser.add_argument("-f", help="ruta al archivo para ser espejado")
-        args = parser.parse_args()
-        if args.f is None:
-            raise ValueError("Error: ingrese una ruta valida")
-    except Exception as e:
-        print(e)
-        return
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", help="ruta al archivo para ser espejado", type=str, required=True)
+    args = parser.parse_args()
 
-    archivo_original = open(args.f, "r")
-    lineas = archivo_original.readlines()
-    archivo_original.close()
+    if not os.path.exists(args.f) or not os.access(args.f, os.R_OK):
+        raise FileNotFoundError(f"No se pudo abrir el archivo: {args.f}")
 
+    with open(args.f, "r") as archivo_original:
+        lineas = archivo_original.readlines()
+
+    largo_linea = []
     for linea in lineas:
+        largo_linea.append(len(linea.encode()))
         os.write(padre_write, linea.encode())
-    largo_lineas_espejadas = []
-    for i in range(len(lineas)):
-        rt = os.fork()
 
+    for i in largo_linea:
+        rt = os.fork()
+        if rt > 0:
+            time.sleep(0.5)
+            continue
         if rt == 0:
-            largo_linea = len(lineas[i].encode())
-            linea = os.read(hijo_read, largo_linea - 1).decode()
-            os.read(hijo_read, 1).decode()
+            linea = os.read(hijo_read, i - 1).decode()
+            os.read(hijo_read, 1)
             linea = linea + "   |   " + linea[::-1] + "\n"
             os.write(hijo_write, linea.encode())
-            largo_lineas_espejadas.append(len(linea.encode()))
+            exit()
 
-    for i in range(len(lineas)):
-        linea = os.read(padre_read, largo_lineas_espejadas[i]).decode()
-        print(linea)
+    linea = os.read(padre_read, 2024).decode()
+    print(linea)
 
     os.close(hijo_read)
     os.close(hijo_write)
