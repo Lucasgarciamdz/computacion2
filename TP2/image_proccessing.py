4dc4747dbfe558bf8a867dd98cfa9fb3f12f6f0e
Lucas Garcia <lu.garcia@alumno.um.edu.ar>
2023-11-13 17:24:10 -0300

    tp2 casi terminado

--- TP2/image_proccessing.py
+++ TP2/image_proccessing.py
@@ -1,19 +1,24 @@
 import logging
 import cv2 as cv
 import asyncio
+import os
 
 
-async def black_and_white(image_url: str):
+async def black_and_white(image_url: str) -> (str, str):
     logging.basicConfig(level=logging.INFO)
     logging.info('image_proccessing.py black_and_white() started')
 
     image = cv.imread(image_url)
     gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    cv.imwrite(image_url, gray_image)
+
+    filename, extension = os.path.splitext(image_url)
+    bw_image_url = f"{filename}_bw{extension}"
+    f"{filename}_bw{extension}"
+
+    cv.imwrite(bw_image_url, gray_image)
 
     logging.info('image_proccessing.py black_and_white() finished')
-    return image_url
-
+    return bw_image_url, f"{filename}_bw{extension}"
 
 if __name__ == '__main__':
-    asyncio.run(black_and_white('test.jpg'))
+    asyncio.run(black_and_white('./test.jpeg'))
--- TP2/main_server.py
+++ TP2/main_server.py
@@ -2,21 +2,26 @@ import argparse
 import logging
 import socket
 import multiprocessing as mp
-from image_processing import black_and_white
+from image_proccessing import black_and_white
+import asyncio
+from iterm2_tools import images
 
 
-def process_image(image_url):
+def process_image(image_url, conn):
     logging.info('Processing image')
-    black_and_white(image_url)
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    result = loop.run_until_complete(black_and_white(image_url))
     logging.info('Image processing finished')
+    conn.send(result)
+    conn.close()
 
 
 def main():
     parser = argparse.ArgumentParser(description="main server")
     parser.add_argument("-p", "--port", type=int, default=8080, help="port")
     parser.add_argument("-i", "--ip", type=str, default="localhost", help="ip")
-    parser.add_argument("-im", "--image", type=str, default="test.jpg", help="image")
-    parser.add_argument("-h", "--help", action="help", help="help")
+    parser.add_argument("-im", "--image", type=str, default="./test.jpeg", help="image")
     args = parser.parse_args()
 
     logging.basicConfig(level=logging.INFO)
@@ -37,22 +42,27 @@ def main():
         conn.send("Connected".encode())
 
         try:
+            try:
+                images.display_image_file(args.image)
+            except Exception as e:
+                logging.error(f"Error displaying image: {e}")
             conn.send("Processing your image".encode())
-            son = mp.Process(target=process_image, args=(args.image,))
+            parent_conn, child_conn = mp.Pipe()
+            son = mp.Process(target=process_image, args=(args.image, child_conn))
             son.start()
+            image_bw, image_path = parent_conn.recv()
+            try:
+                images.display_image_file(image_bw)
+            except Exception as e:
+                logging.error(f"Error displaying image: {e}")
             son.join()
             conn.send("Image processing finished".encode())
-            with open(args.image, "rb") as f:
-                while True:
-                    data = f.read(1024)
-                    if not data:
-                        break
-                    conn.send(data)
+            conn.send(image_path.encode())
         except Exception as e:
             logging.error(f"Error processing image: {e}")
             conn.send(f"Error processing image: {e}".encode())
         finally:
-            client_socket.close()
+            conn.close()
 
 
 if __name__ == '__main__':
